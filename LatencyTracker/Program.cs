3c018fe16a9e79c2a75f4caa911b8bca2087f8f2
Source <65256422+RealSourceOfficial@users.noreply.github.com>
2023-02-06 02:37:06 -0800

    Massive progress     - Revamped save system     - Revamped all failsafes     - Made a bunch of things async     - Introduced dynamic file ouput     - Fixed a bunch more bugs     - More

--- LatencyTracker/Program.cs
+++ LatencyTracker/Program.cs
@@ -1,87 +1,151 @@
 ﻿using System.Net.NetworkInformation;
+using System.Text.Json;
 
-namespace R {
-    public class P {
 
-        public static void Main() {
-            string d = "";
+namespace latencytracker {
+    public class Settings {
+        public string? PreviousSaveSettings { get; set; }
+        public short PingAutoAmount { get; set; }
+    }
+    public class ErrorHandler {
+        public static async Task InvalidAdress() {
+            Console.Clear();
+            Console.WriteLine("Invalid Adress - Press any key to retry");
+            Console.ReadLine();
+            Console.Clear();
+            await Program.Main();
+        }
+    }
+    public class Program {
+        private static readonly string workingFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/SI/LatencyTracker";
+
+        public static async Task Main() {
+            var settings = new Settings();
+            short? PingAutoAmount;
+            string? PreviousSaveSettings = "0";
+            string? defaultOption1 = "";
+            string? defaultOption2 = "";
+            Ping ping = new();
+            List<Settings> data = new() {
+                new Settings() { PreviousSaveSettings = "0", PingAutoAmount = 100 }
+            };
+
+
+
+            if (!Directory.Exists(workingFolder)) {
+                Directory.CreateDirectory(workingFolder);
+
+            }
+            if (!File.Exists(workingFolder + "/settings.json")) {
+
+                await using FileStream dataStream = File.Create(workingFolder + "/settings.json");
+                await JsonSerializer.SerializeAsync(dataStream, data);
+                await dataStream.DisposeAsync();
+
+            } else {
+                try {
+                    await using FileStream dataStream = File.OpenRead(workingFolder + "/settings.json");
+                    Settings? fromFile = await JsonSerializer.DeserializeAsync<Settings>(dataStream);
+                    PingAutoAmount = fromFile?.PingAutoAmount;
+                    PreviousSaveSettings = fromFile?.PreviousSaveSettings ?? PreviousSaveSettings;
+                    await dataStream.DisposeAsync();
+                } catch (JsonException) {
+                    await using FileStream dataStream = File.OpenWrite(workingFolder + "/settings.json");
+                    await JsonSerializer.SerializeAsync(dataStream, data);
+                    await dataStream.DisposeAsync();
+                }
+            }
+
+            if (PreviousSaveSettings != "0") {
+                defaultOption1 = "-- Default";
+            } else {
+                defaultOption2 = "-- Default";
+            }
+
+            string domain = "";
             Console.WriteLine("What domain would you like to ping?");
             Thread.Sleep(1000);
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-            string c = Console.ReadLine();
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
-            if (c != "") {
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-                d = c;
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-            } else if (c == "") {
-                d = "google.com";
-            };
-            Console.Clear();
-            int y = 100;
-            Console.WriteLine("How many times would you like to ping " + d + "? (5-65535) Higher values may take longer.");
-            Thread.Sleep(1000);
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-            string z = Console.ReadLine();
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
-            if (z != "") {
-                if (Int32.TryParse(z, out y) != false) {
-                    if (Int32.Parse(z) > 65535 | Int32.Parse(z) < 2) {
-                        y = 100;
-                    } else {
-                        y = Int32.Parse(z);
-                    };
-                } else {
-                    y = 100;
-                };
+            string? userInputDomain = Console.ReadLine();
+            if (userInputDomain != null) {
+                try {
+                    ping.Send(userInputDomain, 10000);
+                    // 10000ms max trip time
+                    domain = userInputDomain;
+                } catch (PingException) {
+                    await ErrorHandler.InvalidAdress();
+                } catch (ArgumentNullException) {
+                    await ErrorHandler.InvalidAdress();
+                }
             } else {
-                y = 100;
-            };
+                domain = "google.com";
+            }
+
+
             Console.Clear();
+            Console.WriteLine("Ping " + domain + " how many times? (ENTER for Auto or define value 1-65535)");
+            Thread.Sleep(1000);
+            string? input = Console.ReadLine();
+            if (input == null || !Int16.TryParse(input, out short pingAmount) || pingAmount <= 1) {
+                if (settings.PingAutoAmount >= 1) {
+                    pingAmount = settings.PingAutoAmount;
+                } else { pingAmount = 100; }
+            } else {
+                pingAmount = Int16.Parse(input);
+                if (settings.PingAutoAmount == 0) {
+                    var update = new Settings() { PreviousSaveSettings = PreviousSaveSettings, PingAutoAmount = 100 };
+                    await using FileStream dataStream = File.OpenWrite(workingFolder + "/settings.json");
+                    await JsonSerializer.SerializeAsync(dataStream, update);
+                    await dataStream.DisposeAsync();
+                }
+            }
+            Console.Clear();
+
 
 
             Console.WriteLine("Starting...");
-            int i = 0;
-            long[] r = new long[1];
-            Ping p = new();
-            while (true) {
-                i++;
-                Console.WriteLine("Pinging [" + i + "/" + y + "]");
-                PingReply a = p.Send(d, 10000);
+            int pingCounter = 0;
+            long[] results = new long[1];
+
+            while (pingCounter < pingAmount) {
+                pingCounter++;
+                Console.WriteLine("Pinging [" + pingCounter + "/" + pingAmount + "]");
+                PingReply reply = ping.Send(domain, 10000);
                 Thread.Sleep(100);
-                Array.Resize(ref r, r.Length + 1);
-                Console.WriteLine("Processing [" + i + "/" + y + "]");
-                r[r.Length - 1] = a.RoundtripTime;
-                if (r.Length == y) {
+                Array.Resize(ref results, results.Length + 1);
+                Console.WriteLine("Processing [" + pingCounter + "/" + pingAmount + "]");
+
+
+                results[^1] = reply.RoundtripTime;
+                if (results.Length == pingAmount) {
                     Console.Clear();
-                    g(r, d);
-                    Console.Clear();
+                    await ProcessResults(results, domain, defaultOption1, defaultOption2);
+                    return;
                 };
             };
         }
-        public static async void g(long[] v, string d) {
-            long[] x = v.Distinct().ToArray();
-            long m = v.Max();
-            int j = 0;
-            bool k = false;
-            Array.Sort(x);
-            if (m > 100) {
-                foreach (var g in x) {
-                    x[j] = g / 3;
-                    j++;
+        public static async Task ProcessResults(long[] results, string domain, string? defaultOption1, string? defaultOption2) {
+            long[] filteredResults = results.Distinct().ToArray();
+            long resultsMax = results.Max();
+            int counter = 0;
+            bool dividedByThree = false;
+            Array.Sort(filteredResults);
+            if (resultsMax > 100) {
+                foreach (var g in filteredResults) {
+                    filteredResults[counter] = g / 3;
+                    counter++;
 
                 }
-                k = true;
+                dividedByThree = true;
             }
-            foreach (var g in x) {
-                for (var i = 0; i < g; i++) {
-                    if (g > 50) {
+            foreach (var result in filteredResults) {
+                for (var temp = 0; temp < result; temp++) {
+                    if (result > 50) {
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.BackgroundColor = ConsoleColor.Red;
-                    } else if (g > 25) {
+                    } else if (result >= 25) {
                         Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.BackgroundColor = ConsoleColor.Yellow;
-                    } else if (g < 25) {
+                    } else if (result < 25) {
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.BackgroundColor = ConsoleColor.Green;
                     }
@@ -89,40 +153,74 @@ namespace R {
                     Console.ResetColor();
 
                 }
-                if (k == true) {
-                    Console.Write(" " + g * 3 + "ms");
-                } else if (k == false) {
-                    Console.Write(" " + g + "ms");
+                if (dividedByThree == true) {
+                    Console.Write(" " + result * 3 + "ms");
+                } else if (dividedByThree == false && result != 0) {
+                    Console.Write(" " + result + "ms");
                 }
                 Console.WriteLine();
             }
             Console.ResetColor();
             Console.WriteLine();
-            Console.WriteLine("AVG: " + v.Average() + "ms");
-            Console.WriteLine("MAX: " + v.Max() + "ms");
-            Console.WriteLine("MIN: " + v.Where(h => h != 0).Min() + "ms");
+            Console.WriteLine("AVG: " + results.Average() + "ms");
+            Console.WriteLine("MAX: " + results.Max() + "ms");
+            Console.WriteLine("MIN: " + results.Where(temp => temp != 0).Min() + "ms");
             Console.WriteLine("1: Run Another Test | 2: Save Results to File | 3: Exit");
-            string[] o = new string[v.Length + 4];
-            int f = 4;
-            foreach (var l in v) {
-                o[f] = l.ToString();
-                f++;
+            string[] output = new string[results.Length + 4];
+            int outputArrayStart = 4;
+            foreach (var value in results) {
+                output[outputArrayStart] = outputArrayStart - 4 + " " + value.ToString() + "ms";
+                outputArrayStart++;
             }
-            o[1] = d;
-            o[2] = v.Average().ToString();
-            o[3] = v.Max().ToString();
-            o[4] = v.Min().ToString();
-            var u = Console.ReadKey().Key;
-            if (u == ConsoleKey.D1) {
+            output[0] = "DOMAIN: " + domain;
+            output[1] = "AVG: " + results.Average().ToString() + "ms";
+            output[2] = "MAX: " + results.Max().ToString() + "ms";
+            output[3] = "MIN: " + results.Min().ToString() + "ms";
+            var keyPressed = Console.ReadKey().Key;
+            if (keyPressed == ConsoleKey.D1) {
                 Console.Clear();
-                Main();
-            } else if (u == ConsoleKey.D2) {
-                await File.WriteAllLinesAsync(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Results.txt", o);
-                Console.WriteLine("Results saved to /Documents/Results.txt");
+                await Main();
+            } else if (keyPressed == ConsoleKey.D2) {
+                Console.Clear();
+                Console.WriteLine("Export Type");
+                Console.WriteLine($"1: Simple (DOMAIN, AVG, MAX, MIN) {defaultOption1}");
+                Console.WriteLine("2: Advanced (TIMESTAMP, DOMAIN, PINGCOUNT, AVG, MAX, MIN");
+                Console.WriteLine($"3: Custom {defaultOption2}");
+                keyPressed = Console.ReadKey().Key;
+                if (keyPressed == ConsoleKey.D1) {
+                    await File.WriteAllLinesAsync(workingFolder + "/Results.txt", output);
+                    Console.Clear();
+                    Console.WriteLine($"Results saved to {workingFolder}/Results.txt - Press any key to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    await Main();
+                } else if (keyPressed == ConsoleKey.D2) {
+                    string[] output2 = new string[results.Length + 6];
+                    output2[0] = "TIME: " + DateTime.Now.ToString();
+                    output2[1] = "DOMAIN: " + domain;
+                    output2[2] = "COUNT: " + results.Length.ToString();
+                    output2[3] = "AVG: " + results.Average().ToString() + "ms";
+                    output2[4] = "MAX: " + results.Max().ToString() + "ms";
+                    output2[5] = "MIN: " + results.Min().ToString() + "ms";
+                    outputArrayStart = 6;
+                    foreach (var value in results) {
+                        output2[outputArrayStart] = outputArrayStart - 6 + " " + value.ToString() + "ms";
+                        outputArrayStart++;
+                    }
+                    await File.WriteAllLinesAsync(workingFolder + "/Results.txt", output2);
+                    Console.Clear();
+                    Console.WriteLine($"Results saved to {workingFolder}/Results.txt - Press any key to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    await Main();
+                }
+
+
                 Console.ReadLine();
+
                 Console.Clear();
-                Main();
-            } else if (u == ConsoleKey.D3) {
+                await Main();
+            } else if (keyPressed == ConsoleKey.D3) {
                 Environment.Exit(0);
             }
 
